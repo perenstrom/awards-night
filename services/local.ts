87d0d19fe471f98c67d3e0e5c4b7df9c4d2981c5
Per Enström <enstrom.per@gmail.com>
2021-03-27 14:38:38 +0100

    Refactor API

--- services/local.ts
+++ services/local.ts
@@ -5,7 +5,7 @@ export const createBet = async (
   nominationId: string
 ): Promise<Bet> => {
   const url = '/api/bets';
-  const options = {
+  const options: RequestInit = {
     method: 'POST',
     headers: {
       Accept: 'application/json',
@@ -17,7 +17,31 @@ export const createBet = async (
     })
   };
 
-  return new Promise((resolve, reject) => {
+  return apiResult<Bet>(url, options);
+};
+
+export const updateBet = async (
+  betId: string,
+  nominationId: string
+): Promise<Bet> => {
+  const url = '/api/bets';
+  const options: RequestInit = {
+    method: 'PATCH',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json;charset=UTF-8'
+    },
+    body: JSON.stringify({
+      betId,
+      nominationId
+    })
+  };
+
+  return apiResult<Bet>(url, options);
+};
+
+const apiResult = <K>(url: RequestInfo, options: RequestInit): Promise<K> =>
+  new Promise((resolve, reject) => {
     fetch(url, options)
       .then((response) => {
         if (response.status === 200) {
@@ -32,4 +56,3 @@ export const createBet = async (
         reject(error);
       });
   });
-};
