ec109c7b2a904608b294621c66bb50fb923f6fa9
Per Enström <enstrom.per@gmail.com>
2021-10-03 11:34:50 +0200

    Refactor admin page components

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,37 +1,21 @@
-import React, { ReactElement, useRef, useState } from 'react';
+import React from 'react';
 import { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import getRawBody from 'raw-body';
-import {
-  CircularProgress,
-  Typography,
-  TextField,
-  Box,
-  Button,
-  makeStyles,
-  Paper
-} from '@material-ui/core';
+import { Typography, Box } from '@material-ui/core';
 import { Alert } from '@material-ui/lab';
 
 import { PropsWithUser, StatusMessage } from 'types/utilityTypes';
 import { MainContainer } from 'components/MainContainer';
 import { withAdminRequired } from 'lib/withAdminRequired';
 import { saveFilm } from 'lib/saveFilm';
-import { createNominations } from 'services/local';
 import { Category, CategoryId, Film, FilmId, Year } from 'types/nominations';
 import { getCategories, getFilms, getYears } from 'services/airtable';
-import { NominationFields } from 'components/admin/NominationFields';
 import { saveNominations } from 'lib/saveNominations';
-import { parseFormData } from 'utils/parseFormData';
-import { AddNominationsFields, PostBody } from 'types/admin.types';
+import { PostBody } from 'types/admin.types';
 import { AddFilm } from 'components/admin/AddFilm';
-
-const useStyles = makeStyles(() => ({
-  sectionHeading: {
-    paddingTop: 0
-  }
-}));
+import { AddNominations } from 'components/admin/AddNominations';
 
 type Props = {
   statusMessages?: {
@@ -46,75 +30,14 @@ type Props = {
 };
 
 const AdminPage: NextPage<PropsWithUser<Props>> = (props) => {
-  const { sectionHeading } = useStyles(props);
   const {
-    statusMessages: initialStatusMessages,
+    statusMessages,
     availableCategories,
     availableYears,
     availableFilms,
     nominationCount: initialNominationCount
   } = props;
   const router = useRouter();
-  const [statusMessages, setStatusMessages] = useState(initialStatusMessages);
-
-  const nominationCountElement = useRef<HTMLInputElement>(null);
-  const [nominationCount, setNominationCount] = useState(
-    initialNominationCount
-  );
-  const onUpdateNominationCount: React.MouseEventHandler<HTMLButtonElement> = (
-    event
-  ) => {
-    event.preventDefault();
-    const nominationCount = nominationCountElement.current.value;
-
-    if (nominationCount) {
-      setNominationCount(parseInt(nominationCount, 10));
-    }
-  };
-
-  const [addNominationsStatus, setAddNominationsStatus] = useState<
-    'idle' | 'loading'
-  >('idle');
-  const onAddNominationsSubmit: React.FormEventHandler<HTMLFormElement> =
-    async (event) => {
-      event.preventDefault();
-      const target = event.currentTarget;
-      setAddNominationsStatus('loading');
-      setStatusMessages({ ...statusMessages, addNominations: null });
-      const formData = parseFormData<AddNominationsFields>(
-        new FormData(event.currentTarget)
-      );
-      const saveNominationsResult = await createNominations({
-        category: formData.category as CategoryId,
-        year: parseInt(formData.year, 10),
-        films: formData.films as FilmId[],
-        nominees: formData.nominees
-      });
-      setStatusMessages({
-        ...statusMessages,
-        addNominations: saveNominationsResult
-      });
-      if (saveNominationsResult.severity !== 'error') {
-        target.reset();
-      }
-      setAddNominationsStatus('idle');
-    };
-
-  const renderNominationFields = (availableFilms: Film[], count: number) => {
-    let elements: ReactElement[] = [];
-    for (let i = 0; i < count; i++) {
-      elements.push(
-        <NominationFields
-          availableFilms={availableFilms}
-          index={i}
-          first={i === 0}
-          key={`nomination-fields-${i}`}
-        />
-      );
-    }
-
-    return elements;
-  };
 
   return (
     <>
@@ -134,120 +57,14 @@ const AdminPage: NextPage<PropsWithUser<Props>> = (props) => {
           parentStatusMessage={statusMessages.addFilms}
         />
         {availableCategories && availableYears && availableFilms && (
-          <Box mt={2}>
-            <Paper>
-              <Box p={2}>
-                <Typography variant="h2" className={sectionHeading}>
-                  Add nominations
-                </Typography>
-                <Box mt={2}>
-                  <form
-                    action={router.pathname}
-                    method="POST"
-                    onSubmit={onAddNominationsSubmit}
-                  >
-                    <TextField
-                      id="year"
-                      name="year"
-                      label="Year"
-                      variant="outlined"
-                      size="small"
-                      SelectProps={{
-                        native: true
-                      }}
-                      select
-                    >
-                      {availableYears.map((year) => (
-                        <option key={year.id} value={year.year}>
-                          {year.year}
-                        </option>
-                      ))}
-                    </TextField>
-                    <Box ml={1} display="inline">
-                      <TextField
-                        id="category"
-                        name="category"
-                        label="Category"
-                        variant="outlined"
-                        size="small"
-                        SelectProps={{
-                          native: true
-                        }}
-                        select
-                      >
-                        {availableCategories.map((category) => (
-                          <option key={category.id} value={category.id}>
-                            {category.name}
-                          </option>
-                        ))}
-                      </TextField>
-                    </Box>
-                    <Box ml={2} display="inline">
-                      <TextField
-                        id="nominationCount"
-                        name="nominationCount"
-                        label="Nomination count"
-                        variant="outlined"
-                        size="small"
-                        defaultValue={nominationCount}
-                        SelectProps={{
-                          native: true
-                        }}
-                        select
-                        inputRef={nominationCountElement}
-                      >
-                        {[3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((count) => (
-                          <option key={`category-count-${count}`} value={count}>
-                            {`${count} nominations`}
-                          </option>
-                        ))}
-                      </TextField>
-                    </Box>
-                    <Box ml={1} display="inline">
-                      <Button
-                        name="action"
-                        value="changeNominationCount"
-                        variant="contained"
-                        color="primary"
-                        type="submit"
-                        onClick={onUpdateNominationCount}
-                        disabled={addNominationsStatus === 'loading'}
-                        disableElevation
-                      >
-                        Update
-                      </Button>
-                    </Box>
-                    {renderNominationFields(availableFilms, nominationCount)}
-                    <Box mt={2}>
-                      <Button
-                        name="action"
-                        value="addNominations"
-                        variant="contained"
-                        color="primary"
-                        type="submit"
-                        disabled={addNominationsStatus === 'loading'}
-                        disableElevation
-                      >
-                        Save
-                      </Button>
-                    </Box>
-                    {addNominationsStatus === 'loading' && (
-                      <Box mt={2.5}>
-                        <CircularProgress size={'2rem'} />
-                      </Box>
-                    )}
-                  </form>
-                  {statusMessages?.addNominations && (
-                    <Box mt={2}>
-                      <Alert severity={statusMessages.addNominations.severity}>
-                        {statusMessages.addNominations.message}
-                      </Alert>
-                    </Box>
-                  )}
-                </Box>
-              </Box>
-            </Paper>
-          </Box>
+          <AddNominations
+            submitAction={router.pathname}
+            parentStatusMessage={statusMessages.addNominations}
+            initialNominationCount={initialNominationCount}
+            availableCategories={availableCategories}
+            availableFilms={availableFilms}
+            availableYears={availableYears}
+          />
         )}
       </MainContainer>
     </>
