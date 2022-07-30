11ce0202e7ecc0db77a5188d8048493aeb35cebe
Per Enström <enstrom.per@gmail.com>
2022-07-30 12:20:53 +0200

    Add new design test

---
+++ components/NominatedFilmDashboard.tsx
@@ -0,0 +1,84 @@
+import { styled } from '@mui/material/styles';
+import { defaultStyledOptions } from 'utils/mui';
+
+interface Size {
+  width: number;
+  height: number;
+  restrictedBy: 'height' | 'width';
+}
+interface NominationsProps {
+  readonly size: Size | null;
+}
+
+const getSize = (size: Size) => {
+  switch (size.restrictedBy) {
+    case 'height':
+      return {
+        height: `calc(calc(${size.height - 1}px - 2rem)/3)`,
+        width: 'unset'
+      };
+    case 'width':
+      return {
+        height: 'unset',
+        width: `calc(calc(${size.width - 1}px - 1rem)/2)`
+      };
+  }
+};
+
+const Nomination = styled(
+  'div',
+  defaultStyledOptions<NominationsProps>(['size'])
+)<NominationsProps>`
+  aspect-ratio: 9 / 4;
+  width: ${({ size }) => (size ? getSize(size).width : '40%')};
+  height: ${({ size }) => (size ? getSize(size).height : '25%')};
+  box-sizing: border-box;
+  background: #2c2c2c;
+  border-radius: 8px;
+  padding: 0.5em;
+
+  display: grid;
+  grid-template-columns: 29fr 71fr;
+  grid-template-rows: min-content min-content auto min-content;
+  grid-template-areas:
+    'poster title'
+    'poster nominee'
+    'poster .'
+    'poster bets';
+  grid-gap: 0.5em;
+
+  div {
+    background: lightgreen;
+  }
+
+  #poster {
+    grid-area: poster;
+  }
+  #title {
+    grid-area: title;
+  }
+  #nominee {
+    grid-area: nominee;
+  }
+  #bets {
+    grid-area: bets;
+  }
+`;
+
+const Image = styled('img')`
+  display: block;
+  width: 100%;
+`;
+
+export const NominatedFilm: React.FC<NominationsProps> = ({ size }) => {
+  return (
+    <Nomination size={size}>
+      <div id="poster">
+        <Image src="https://image.tmdb.org/t/p/w342/y89kFMNYXNKMdlZjR2yg7nQtcQH.jpg" />
+      </div>
+      <div id="title">title</div>
+      <div id="nominee">nominee</div>
+      <div id="bets">bets</div>
+    </Nomination>
+  );
+};
---
+++ pages/[year]/new-design.tsx
@@ -0,0 +1,111 @@
+import { styled } from '@mui/material/styles';
+import { FilmPoster } from 'components/FilmPoster';
+import { NominatedFilm } from 'components/NominatedFilmDashboard';
+import { NextPage } from 'next';
+import React from 'react';
+
+const MainWrapper = styled('div')`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  font-size: 16px;
+`;
+
+const Sidebar = styled('div')`
+  flex-basis: 18%;
+  background: #363636;
+  flex-grow: 0;
+`;
+
+const Main = styled('div')`
+  width: 376px;
+  background: linear-gradient(180deg, #24242e 0%, #111115 24.3%);
+  flex-grow: 1;
+
+  display: flex;
+  flex-direction: column;
+  padding: 3em;
+`;
+
+const Heading = styled('h1')`
+  font-family: 'Inter', sans-serif;
+  font-weight: 300;
+  font-size: 3.5em;
+  color: #e5e7f8;
+  margin: 0;
+  padding-bottom: 1rem;
+`;
+
+interface Size {
+  width: number;
+  height: number;
+  restrictedBy: 'height' | 'width';
+}
+
+const NominationsArea = styled('div')`
+  height: 100%;
+  font-size: 1em;
+  gap: 1em;
+
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  align-content: flex-start;
+  justify-content: flex-start;
+`;
+
+const AREA_ID = 'nominations-area';
+
+const getNominationSize = (): Size | null => {
+  if (typeof window !== 'undefined') {
+    const $nominationsArea = document.getElementById(AREA_ID);
+
+    if ($nominationsArea) {
+      const emSize = parseInt(
+        getComputedStyle($nominationsArea).fontSize.slice(0, -2),
+        10
+      );
+
+      const nominationsAreaWidth = $nominationsArea.offsetWidth;
+      const nominationsAreaHeight = $nominationsArea.offsetHeight;
+
+      const contentWidth = nominationsAreaWidth - emSize;
+      const contentHeight = nominationsAreaHeight - 2 * emSize;
+
+      const contentRatio = contentWidth / contentHeight;
+      const cardRatio = 3 / 2;
+
+      const restrictedBy = contentRatio >= cardRatio ? 'height' : 'width';
+
+      return {
+        width: nominationsAreaWidth,
+        height: nominationsAreaHeight,
+        restrictedBy: restrictedBy
+      };
+    }
+  }
+
+  return null;
+};
+
+const NewDesignPage: NextPage<{}> = () => {
+  const nominationSize = getNominationSize();
+
+  return (
+    <MainWrapper>
+      <Sidebar>Sidebar</Sidebar>
+      <Main>
+        <Heading>Best Actress</Heading>
+        <NominationsArea id={AREA_ID}>
+          <NominatedFilm size={nominationSize} />
+          <NominatedFilm size={nominationSize} />
+          <NominatedFilm size={nominationSize} />
+          <NominatedFilm size={nominationSize} />
+          <NominatedFilm size={nominationSize} />
+        </NominationsArea>
+      </Main>
+    </MainWrapper>
+  );
+};
+
+export default NewDesignPage;
