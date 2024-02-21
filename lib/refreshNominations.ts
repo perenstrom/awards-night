721983198b015e0db6002e475db0c8e78725147b
Per Enström <enstrom.per@gmail.com>
2024-02-21 17:22:00 +0100

    Remove failing files

--- lib/refreshNominations.ts
+++
@@ -1,9 +0,0 @@
-import { getNominations } from 'services/prisma';
-import { Nomination, Year } from 'types/nominations';
-import { prismaContext } from './prisma';
-
-export const refreshNominations = async (year: Year): Promise<Nomination[]> => {
-  const nominations = await getNominations(year.nominations, prismaContext);
-
-  return nominations;
-};
