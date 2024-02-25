a807e5a2729963ee86492d797a0a9530951e74e2
Per Enström <enstrom.per@gmail.com>
2024-02-25 12:55:54 +0100

    Move auth check to page

--- app/me/layout.tsx
+++ app/me/layout.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
-import { getSession, withPageAuthRequired } from '@auth0/nextjs-auth0';
-import { redirect } from 'next/navigation';
 import YearList from 'components/dashboard/YearList';
 import styles from './me.module.scss';
 
-export default withPageAuthRequired(
-  async function MeLayout({ children }: { children?: React.ReactNode }) {
-    const session = await getSession();
-    if (!session) redirect('/');
-
-    return (
-      <div className={styles.mainWrapper}>
-        <YearList />
-        {children}
-      </div>
-    );
-  },
-  { returnTo: '/me' }
-);
+export default async function MeLayout({
+  children
+}: {
+  children?: React.ReactNode;
+}) {
+  return (
+    <div className={styles.mainWrapper}>
+      <YearList />
+      {children}
+    </div>
+  );
+}
--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,9 +1,16 @@
+import { getSession, withPageAuthRequired } from '@auth0/nextjs-auth0';
 import { Metadata } from 'next';
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'My predictions – Awards Night'
 };
 
-export default async function Page() {
-  return <></>;
-}
+export default withPageAuthRequired(
+  async function Page() {
+    const session = await getSession();
+    if (!session) redirect('/');
+    return <></>;
+  },
+  { returnTo: '/me' }
+);
