640160a7ff21f9e7e2145a60a7a518a5b1b798cb
Per Enström <enstrom.per@gmail.com>
2024-03-05 20:35:16 +0100

    Add no-margin version of typography

--- components/base/Typography.tsx
+++ components/base/Typography.tsx
@@ -4,26 +4,83 @@ import styles from './Typography.module.scss';
 interface Props {
   children: React.ReactNode;
   variant: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'body';
+  noMargin?: boolean;
 }
 
 export const Typography = (props: Props) => {
-  const { children, variant } = props;
+  const { children, variant, noMargin = false } = props;
 
   switch (variant) {
     case 'h1':
-      return <h1 className={clsx(styles.base, styles.h1)}>{children}</h1>;
+      return (
+        <h1
+          className={clsx(styles.base, styles.h1, {
+            [styles.margin]: !noMargin
+          })}
+        >
+          {children}
+        </h1>
+      );
     case 'h2':
-      return <h2 className={clsx(styles.base, styles.h2)}>{children}</h2>;
+      return (
+        <h2
+          className={clsx(styles.base, styles.h2, {
+            [styles.margin]: !noMargin
+          })}
+        >
+          {children}
+        </h2>
+      );
     case 'h3':
-      return <h3 className={clsx(styles.base, styles.h3)}>{children}</h3>;
+      return (
+        <h3
+          className={clsx(styles.base, styles.h3, {
+            [styles.margin]: !noMargin
+          })}
+        >
+          {children}
+        </h3>
+      );
     case 'h4':
-      return <h4 className={clsx(styles.base, styles.h4)}>{children}</h4>;
+      return (
+        <h4
+          className={clsx(styles.base, styles.h4, {
+            [styles.margin]: !noMargin
+          })}
+        >
+          {children}
+        </h4>
+      );
     case 'h5':
-      return <h5 className={clsx(styles.base, styles.h5)}>{children}</h5>;
+      return (
+        <h5
+          className={clsx(styles.base, styles.h5, {
+            [styles.margin]: !noMargin
+          })}
+        >
+          {children}
+        </h5>
+      );
     case 'h6':
-      return <h6 className={clsx(styles.base, styles.h6)}>{children}</h6>;
+      return (
+        <h6
+          className={clsx(styles.base, styles.h6, {
+            [styles.margin]: !noMargin
+          })}
+        >
+          {children}
+        </h6>
+      );
 
     default:
-      return <p className={clsx(styles.base, styles.body)}>{children}</p>;
+      return (
+        <p
+          className={clsx(styles.base, styles.body, {
+            [styles.margin]: !noMargin
+          })}
+        >
+          {children}
+        </p>
+      );
   }
 };
