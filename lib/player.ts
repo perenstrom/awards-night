e7d1530a3cb9c464317902a8fb940c54afa4888c
Per Enström <enstrom.per@gmail.com>
2024-03-05 21:27:48 +0100

    Debug loggedinplayer check

--- lib/player.ts
+++ lib/player.ts
@@ -3,6 +3,7 @@ import { getPlayerByAuth0Id } from 'services/prisma/players';
 
 export const getLoggedInPlayer = async () => {
   const session = await getSession();
+  console.log(JSON.stringify(session, null, 2));
   const user = session?.user ?? null;
   const auth0id = user?.sub as string;
 
@@ -11,6 +12,8 @@ export const getLoggedInPlayer = async () => {
 
     return null;
   }
+  const player = await getPlayerByAuth0Id(auth0id);
+  console.log(JSON.stringify(player, null, 2));
 
-  return await getPlayerByAuth0Id(auth0id);
+  return player;
 };
