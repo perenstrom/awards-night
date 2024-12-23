9f0c08f50f99d9fa6d810da47d178f779f8f6d72
Per Enström <enstrom.per@gmail.com>
2024-12-23 15:29:48 +0100

    Update styles for prediction page

--- components/dashboard/YearCard.tsx
+++ components/dashboard/YearCard.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { Bet, NominationData } from 'types/nominations';
 import styles from './YearCard.module.scss';
 import { YearCardWrapper } from './YearCardWrapper';
+import { YearCardChevron } from './YearCardChevron';
 
 type YearState =
   | 'open' // year.bettingOpen && bets.length === 0
@@ -215,18 +216,7 @@ export const YearCard: React.FC<Props> = ({ nominationData, bets }) => {
         </h2>
         <div className={styles.textAndChevronWrapper}>
           <p className={styles.stateText}>{stateText}</p>
-          <svg
-            width="7"
-            height="13"
-            viewBox="0 0 7 13"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M6.76502 5.92612C7.07752 6.24351 7.07752 6.75894 6.76502 7.07632L1.96502 11.9513C1.65252 12.2687 1.14502 12.2687 0.83252 11.9513C0.52002 11.6339 0.52002 11.1185 0.83252 10.8011L5.06752 6.49995L0.835019 2.19878C0.522519 1.8814 0.522519 1.36597 0.835019 1.04858C1.14752 0.731201 1.65502 0.731201 1.96752 1.04858L6.76752 5.92358L6.76502 5.92612Z"
-              fill="#737373"
-            />
-          </svg>
+          <YearCardChevron year={year.year} />
         </div>
         <div className={styles.progressWrapper}>
           <progress
