cc1a618ae7f0ef33dad751335be352d244468f97
Per Enström <enstrom.per@gmail.com>
2024-12-23 14:46:08 +0100

    Fix types

--- services/prisma/prisma.types.ts
+++ services/prisma/prisma.types.ts
@@ -1,18 +1,19 @@
 import { Prisma, PrismaClient } from '@prisma/client';
 
-const yearWithNominationsAndCategories = Prisma.validator<Prisma.YearArgs>()({
-  include: { nominations: true, yearsCategories: true }
-});
+const yearWithNominationsAndCategories =
+  Prisma.validator<Prisma.YearDefaultArgs>()({
+    include: { nominations: true, yearsCategories: true }
+  });
 export type YearWithNominationsAndCategories = Prisma.YearGetPayload<
   typeof yearWithNominationsAndCategories
 >;
 
-const playerWithBets = Prisma.validator<Prisma.PlayerArgs>()({
+const playerWithBets = Prisma.validator<Prisma.PlayerDefaultArgs>()({
   include: { bets: true }
 });
 export type PlayerWithBets = Prisma.PlayerGetPayload<typeof playerWithBets>;
 
-const categoryWithNominations = Prisma.validator<Prisma.CategoryArgs>()({
+const categoryWithNominations = Prisma.validator<Prisma.CategoryDefaultArgs>()({
   include: { nominations: true }
 });
 export type CategoryWithNominations = Prisma.CategoryGetPayload<
