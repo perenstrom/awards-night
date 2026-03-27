c053e0a55685cb4eb595bf56912d587dd7470ceb
Per Enström <enstrom.per@gmail.com>
2026-03-27 16:55:35 +0100

    Migrate button styles

--- components/base/Button.tsx
+++ components/base/Button.tsx
@@ -1,5 +1,4 @@
 import Link, { LinkProps } from 'next/link';
-import styles from './Button.module.scss';
 
 type Props =
   | (React.ButtonHTMLAttributes<HTMLButtonElement> & {
@@ -11,16 +10,21 @@ type Props =
     });
 
 export const Button = (props: Props) => {
+  const wrapperClassName =
+    'h-10 rounded border-0 bg-text-primary p-0 shadow-[var(--shadow-elevation-medium)]';
+  const buttonClassName =
+    'block h-full w-full cursor-pointer border-0 bg-transparent px-4 py-2 text-black no-underline max-md:px-2 max-md:py-[0.2rem] max-md:text-[0.9rem]';
+
   if (props.element === 'a') {
     return (
-      <div className={styles.wrapper}>
-        <Link className={styles.button} {...props} />
+      <div className={wrapperClassName}>
+        <Link className={buttonClassName} {...props} />
       </div>
     );
   } else {
     return (
-      <div className={styles.wrapper}>
-        <button className={styles.button} {...props} />
+      <div className={wrapperClassName}>
+        <button className={buttonClassName} {...props} />
       </div>
     );
   }
